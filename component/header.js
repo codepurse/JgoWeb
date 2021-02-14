250208a9055afff346d067574aefdee2ae081501
codepurse <47669262+codepurse@users.noreply.github.com>
2021-02-14 11:56:07 +0800

    update

--- component/header.js
+++ component/header.js
@@ -8,7 +8,7 @@ export class header extends Component {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>Jgo Website</title>
         <meta http-equiv="Content-Security-Policy" content="default-src gap://ready file://* *; style-src 'self' http://* https://* 'unsafe-inline'; script-src 'self' http://* https://* 'unsafe-inline'"/>
-        <meta name="referrer" content="no-referrer-when-downgrade" />
+        <meta name="referrer" content="origin" />
 
         <link rel="stylesheet" href="Css/index.css"></link>
         <link
