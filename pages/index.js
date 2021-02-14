4ca45397d53ca46a2d83b74fe06d2e16cae737f6
codepurse <47669262+codepurse@users.noreply.github.com>
2021-02-14 10:45:28 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -677,7 +677,7 @@ function driver() {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta http-equiv="Content-Security-Policy" content="default-src https:"/>
+       
 
         <meta charSet="utf-8" />
         <meta name="description" content="JGO Delivery Maasahan! Abangan!" />
