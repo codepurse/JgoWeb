682a75975f727fc8d1462af3a0bb47e39676d2c4
codepurse <47669262+codepurse@users.noreply.github.com>
2021-02-16 17:19:27 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,22 @@ module.exports = {
     API_BASE_URL: "https://staging-api.jgo.com.ph",
       customKey: 'my-value',
     },
+    async headers() {
+      return [
+        {
+          // matching all API routes
+          source: "/api/:path*",
+          headers: [
+            { key: "X-XSS-Protection", value: "1; mode=block" },
+            { key: "X-Content-Type-Options", value: "nosniff" },
+            { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          ]
+        }
+      ]
+    },
+      env: {
+      API_BASE_URL: "https://staging-api.jgo.com.ph",
+        customKey: 'my-value',
+      },
   }
+
